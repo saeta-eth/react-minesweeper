add5a7f35a3d70baf1d031eef8faf4d2126eabd5
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 12:00:36 -0300

    Added Row component

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -3,17 +3,22 @@ import Checkbox from '../Checkbox';
 
 import './index.css';
 
-const Row = ({ cols, row }) => (
+const Row = ({ cols, row, grid, handleClick, handleRightClick }) => (
   <div className="row-mw">
-    {new Array(cols).fill(0).map((cur, col) => {
-      return (
-        <Checkbox
-          key={`${row}-${col}`}
-          row={row}
-          col={col}
-        />
-      );
-    })}
+    {
+      new Array(cols).fill(0).map((cur, col) => {
+        return (
+          <Checkbox
+            key={`${row}-${col}`}
+            grid={grid}
+            row={row}
+            col={col}
+            handleClick={handleClick}
+            handleRightClick={handleRightClick}
+          />
+        );
+      })
+    }
   </div>
 );
 
