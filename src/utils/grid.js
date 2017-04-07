6ea8d5f0f4f18510f8c39fdf1c48a244481c493f
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 19:21:09 -0300

    Added fillWarningNumbers function

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -20,11 +20,12 @@ const fillMultiArray = function fillMultiArray(x,y, value) {
   return deepClone(new Array(x).fill(new Array(y).fill(value)));
 }
 
-const fillBombGrid = function fillBombGrid(CANT_POSITIONS, ROW_NUMBER, COLS_NUMBER) {
-  const positionBombs = fillRandomBoolean(CANT_POSITIONS, ROW_NUMBER);
+const fillMultiArray = function fillMultiArray(x,y, value) {
+  return deepClone(new Array(x).fill(new Array(y).fill(value)));
+}
+const fillBombGrid = function fillBombGrid(positionBombs, ROW_NUMBER, COLS_NUMBER) {
   const arrayBomb = fillMultiArray(ROW_NUMBER, COLS_NUMBER, 0);
-  
-  for (let bomb = 0; bomb < CANT_POSITIONS; bomb += 2) {
+  for (let bomb = 0; bomb < (positionBombs.length / 2); bomb += 2) {
     const position = positionBombs.slice(bomb, bomb + 2)
     arrayBomb[position[0]][position[1]] = 1;
   }
@@ -32,8 +33,44 @@ const fillBombGrid = function fillBombGrid(CANT_POSITIONS, ROW_NUMBER, COLS_NUMB
   return arrayBomb;
 }
 
+const fillWarningNumbers = function fillWarningNumbers(grid, positionBombs) {
+  for (let bomb = 0; bomb < (positionBombs.length / 2); bomb += 2) {
+    const position = positionBombs.slice(bomb, bomb + 2);
+    // TOP
+    if (typeof grid[position[0]-1] !== 'undefined' && typeof grid[position[0]-1][position[1]-1] !== 'undefined') {
+      grid[position[0]-1][position[1]-1].status = typeof grid[position[0]-1][position[1]-1].status === 'number' ? grid[position[0]-1][position[1]-1].status+1  : 1;
+    }
+    if (typeof grid[position[0]][position[1]-1] !== 'undefined') {
+      grid[position[0]][position[1]-1].status = typeof grid[position[0]][position[1]-1].status === 'number' ? grid[position[0]][position[1]-1].status+1 : 1;
+    }
+    if (typeof grid[position[0]+1] !== 'undefined' && typeof grid[position[0]+1][position[1]-1] !== 'undefined') { 
+      grid[position[0]+1][position[1]-1].status = typeof grid[position[0]+1][position[1]-1].status === 'number' ? grid[position[0]+1][position[1]-1].status+1 : 1;
+    }
+    // MIDDLE
+    if (typeof grid[position[0]-1] !== 'undefined' && typeof grid[position[0]-1][position[1]] !== 'undefined') {
+      grid[position[0]-1][position[1]].status = typeof grid[position[0]-1][position[1]].status === 'number' ? grid[position[0]-1][position[1]].status+1 : 1;
+    }
+    if (typeof grid[position[0]+1] !== 'undefined' && typeof grid[position[0]+1][position[1]] !== 'undefined') {
+      grid[position[0]+1][position[1]].status = typeof grid[position[0]+1][position[1]].status === 'number' ? grid[position[0]+1][position[1]].status+1 : 1;
+    }
+    // BOTTOM
+    if (typeof grid[position[0]-1] !== 'undefined' && typeof grid[position[0]-1][position[1]+1] !== 'undefined') {
+      grid[position[0]-1][position[1]+1].status = typeof grid[position[0]-1][position[1]+1].status === 'number' ? grid[position[0]-1][position[1]+1].status+1 : 1;
+    }
+    if (typeof grid[position[0]][position[1]+1] !== 'undefined') {
+      grid[position[0]][position[1]+1].status = typeof grid[position[0]][position[1]+1].status === 'number' ? grid[position[0]][position[1]+1].status+1 : 1;
+    }
+    if (typeof grid[position[0]+1] !== 'undefined' && typeof grid[position[0]+1][position[1]+1] !== 'undefined') {
+      grid[position[0]+1][position[1]+1].status = typeof grid[position[0]+1][position[1]+1].status === 'number' ? grid[position[0]+1][position[1]+1].status+1 : 1;
+    }
+  }
+  return grid;
+}
+
 export {
+  fillRandomBoolean,
   fillBombGrid,
   fillMultiArray,
+  fillWarningNumbers,
   deepClone
 }
