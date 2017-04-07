6f887f542ccd9b19fd74fddc4f902b99702dff4a
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 11:52:43 -0300

    Added bomb grid and game grid as initial state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import configureStore from './store/configure';
 
+import { ROW_NUMBER, COLS_NUMBER, checkboxFlow, CANT_POSITIONS }  from './constants';
+import { fillBombGrid, fillMultiArray } from './utils/grid';
+
+const bomb = fillBombGrid(CANT_POSITIONS, ROW_NUMBER, COLS_NUMBER);
+const grid = fillMultiArray(ROW_NUMBER, COLS_NUMBER, checkboxFlow.CHECKBOX_INITIAL);
+
 const initialState = {
-  config: {rows: 10, cols: 10}
+  config: { rows: ROW_NUMBER, cols: COLS_NUMBER },
+  bomb: bomb,
+  grid: grid
 }
 
 const store = configureStore(initialState);
