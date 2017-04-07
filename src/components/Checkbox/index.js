fe878f255da8289b953d33a795b83f0c63dd19e1
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 19:26:13 -0300

    Changed to use status field of the object

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -6,10 +6,11 @@ import './index.css';
 
 const Checkbox = ({ row, col, grid, handleClick, handleRightClick }) => (
   <div 
-    className={`checkbox-mw ${grid[row][col] === checkboxFlow.CHECKBOX_PRESSED ? 'isPressed': ''}` }
+    className={`checkbox-mw ${grid[row][col].status === checkboxFlow.CHECKBOX_PRESSED ? 'isPressed': ''}` }
     onClick={handleClick.bind(this, row, col)}
     onContextMenu={handleRightClick.bind(this, row, col)}
   >
+    { typeof grid[row][col].status === 'number' ? grid[row][col].status : '' }
   </div>
 );
 
--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -6,7 +6,7 @@ export default (state = [], action) => {
   switch (action.type) {
     case actionTypes.CHANGE_GRID:
       let previousState = deepClone(state);
-      previousState[action.col][action.row] = action.value;
+      previousState[action.col][action.row].status = action.value;
       return previousState;
     default:
       return state;
