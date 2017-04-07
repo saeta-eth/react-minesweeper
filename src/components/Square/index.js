945a543fec11a4955100f8acb9e574428bab90d5
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 12:01:09 -0300

    Added Square component

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -3,14 +3,17 @@ import Row from '../Row';
 
 import './index.css';
 
-const Square = ({rows, cols}) => (
+const Square = ({rows, cols, grid, handleClick, handleRightClick}) => (
   <div className="square-mw">
     {new Array(rows).fill(0).map((cur, row) => {
       return (
         <Row
           key={`row-${row}`}
+          grid={grid}
           cols={cols}
           row={row}
+          handleClick={handleClick}
+          handleRightClick={handleRightClick}
         />
       )
     })}
