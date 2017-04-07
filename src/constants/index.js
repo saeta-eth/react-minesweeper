8b4f917c2f42885f830abe46052d21e114072413
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 12:02:20 -0300

    Added some constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,24 @@ import { createConstants } from '../utils/constants.js';
 
 const actionTypes = createConstants(
   'CHANGE_CONFIG',
+  'CHANGE_GRID',
 );
 
+const checkboxFlow = createConstants(
+  'CHECKBOX_INITIAL',
+  'CHECKBOX_PRESSED',
+  'CHECKBOX_FLAG',
+);
+
+const ROW_NUMBER = 20;
+const COLS_NUMBER = 20;
+
+const CANT_POSITIONS = 40;
+
 export {
-  actionTypes
+  actionTypes,
+  checkboxFlow,
+  ROW_NUMBER,
+  COLS_NUMBER,
+  CANT_POSITIONS
 }
