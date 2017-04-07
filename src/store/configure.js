fcc5091fb4b3c1af848a42fae613ff000becfa81
slorenzo <SebastianLorenzo@gmail.com>
2017-04-06 23:01:40 -0300

    Implemented thunk to the store

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,10 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-import {createStore} from 'redux';
 
 export default (initialState) => {
   return createStore(
     rootReducer,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
   );
 };
