6d57731159760cb783829f600140be6691df82b0
slorenzo <SebastianLorenzo@gmail.com>
2017-04-07 19:24:23 -0300

    Created multidimensional array with object as default value

--- src/index.js
+++ src/index.js
@@ -5,15 +5,26 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configure';
 
 import { ROW_NUMBER, COLS_NUMBER, checkboxFlow, CANT_POSITIONS }  from './constants';
-import { fillBombGrid, fillMultiArray } from './utils/grid';
+import { fillBombGrid, fillMultiArray, fillWarningNumbers, fillRandomBoolean } from './utils/grid';
 
-const bomb = fillBombGrid(CANT_POSITIONS, ROW_NUMBER, COLS_NUMBER);
-const grid = fillMultiArray(ROW_NUMBER, COLS_NUMBER, checkboxFlow.CHECKBOX_INITIAL);
+
+const positionBombs = fillRandomBoolean(CANT_POSITIONS, ROW_NUMBER);
+const bomb = fillBombGrid(positionBombs, ROW_NUMBER, COLS_NUMBER);
+
+const gridWithWarningNumbers = fillWarningNumbers(
+  fillMultiArray(ROW_NUMBER, COLS_NUMBER, {
+    status: checkboxFlow.CHECKBOX_INITIAL,
+    visibility: false,
+  }),
+  positionBombs
+);
+
+console.log('gridWithWarningNumbers', gridWithWarningNumbers)
 
 const initialState = {
   config: { rows: ROW_NUMBER, cols: COLS_NUMBER },
   bomb: bomb,
-  grid: grid
+  grid: gridWithWarningNumbers
 }
 
 const store = configureStore(initialState);
