604c0668b6bd606625709a4d90fae65a8f90b3f0
slorenzo <SebastianLorenzo@gmail.com>
2017-04-08 12:23:32 -0300

    Changed cellStatus constants rather than checkboxFlow

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { checkboxFlow }  from '../../constants'
+import { cellStatus }  from '../../constants'
 
 
 import './index.css';
 
 const Checkbox = ({ row, col, grid, handleClick, handleRightClick }) => (
   <div 
-    className={`checkbox-mw ${grid[row][col].status === checkboxFlow.CHECKBOX_PRESSED ? 'isPressed': ''}` }
+    className={`checkbox-mw ${grid[row][col].status === cellStatus.CELL_PRESSED  || grid[row][col].visibility ? 'isPressed': ''}` }
     onClick={handleClick.bind(this, row, col)}
     onContextMenu={handleRightClick.bind(this, row, col)}
   >
     { grid[row][col].visibility ? grid[row][col].status : '' }
+    { grid[row][col].status === cellStatus.CELL_FLAG || grid[row][col].status === cellStatus.CELL_BOMB_FLAG ? 'F' : ''}
   </div>
 );
 
