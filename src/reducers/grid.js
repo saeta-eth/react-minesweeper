31ccc8475898ebd86743b7596a7689a0c7c3e3bf
slorenzo <SebastianLorenzo@gmail.com>
2017-04-08 12:19:42 -0300

    Added different actions types

--- src/reducers/grid.js
+++ src/reducers/grid.js
@@ -1,11 +1,14 @@
 import { actionTypes } from '../constants';
-import { deepClone } from '../utils/grid';
-
+import { deepClone, findHowMuchExpand } from '../utils/grid';
 
 export default (state = [], action) => {
+  let previousState = deepClone(state);
   switch (action.type) {
-    case actionTypes.CHANGE_GRID:
-      let previousState = deepClone(state);
+    case actionTypes.LEFT_CLICK_CELL:
+      previousState[action.col][action.row].status = action.value;
+      previousState = findHowMuchExpand(previousState, action.col, action.row, action.value);
+      return previousState;
+    case actionTypes.RIGHT_CLICK_CELL:
       previousState[action.col][action.row].status = action.value;
       return previousState;
     default:
