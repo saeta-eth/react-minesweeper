b913451a2ca4b74d05f6da908ca4a72fd79ad3a8
slorenzo <SebastianLorenzo@gmail.com>
2017-04-08 18:07:42 -0300

    Added Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.css';
+
+const Header = () => (
+  <div className="header">
+    <h2 className="title">Minesweeper Challenge</h2>
+  </div>
+);
+
+export default Header;
