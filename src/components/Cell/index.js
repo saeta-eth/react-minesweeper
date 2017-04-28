a11ec0a8531a4c4e9c865791632e65cf1efca92a
slorenzo <SebastianLorenzo@gmail.com>
2017-04-28 11:17:56 -0300

    Improved code as the cell shows data

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -6,12 +6,13 @@ import './index.css';
 
 const Cell = ({ row, col, grid, handleClick, handleRightClick }) => (
   <div 
-    className={`checkbox-mw ${grid[row][col].status === cellStatus.CELL_PRESSED  || grid[row][col].visibility ? 'isPressed': ''}` }
+    className={`checkbox-mw ${grid[row][col].status === cellStatus.CELL_PRESSED ? 'isPressed': ''}` }
     onClick={handleClick.bind(this, row, col)}
     onContextMenu={handleRightClick.bind(this, row, col)}
   >
-    { grid[row][col].visibility  ? grid[row][col].warning : '' }
-    { grid[row][col].status === cellStatus.CELL_FLAG || grid[row][col].status === cellStatus.CELL_MINE_FLAG ? ReactEmoji.emojify(':triangular_flag_on_post:') : ''}
+    { (grid[row][col].status === cellStatus.CELL_PRESSED && grid[row][col].warning > 0) ? grid[row][col].warning : '' }
+    { (grid[row][col].status === cellStatus.CELL_FLAG || grid[row][col].status === cellStatus.CELL_MINE_FLAG) ? ReactEmoji.emojify(':triangular_flag_on_post:') : ''}
+    { (grid[row][col].status === cellStatus.CELL_QUESTION_MARK || grid[row][col].status === cellStatus.CELL_MINE_CELL_QUESTION_MARK) ? ReactEmoji.emojify(':question:') : ''}
   </div>
 );
 
