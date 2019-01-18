d76753e1e4192c72a3db5500ed68061b5f865e59
slorenzo <SebastianLorenzo@gmail.com>
2019-01-18 17:48:50 -0300

    Changed fillWarningNumbers util function

--- src/utils/fill-warning-numbers.js
+++ src/utils/fill-warning-numbers.js
@@ -1,11 +1,67 @@
 import { cellStatus } from '../constants';
 
 const fillMineGrid = (grid, positionMines) => {
+  // http://stackoverflow.com/a/2036743/1741027
   for (let mine = 0; mine < positionMines.length; mine += 2) {
     const [col, row] = positionMines.slice(mine, mine + 2);
-    grid[col][row].status = cellStatus.CELL_MINE;
+    // TOP
+    if (
+      typeof grid[col - 1] !== 'undefined' &&
+      typeof grid[col - 1][row - 1] !== 'undefined' &&
+      grid[col - 1][row - 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col - 1][row - 1].warning++;
+    }
+    if (
+      typeof grid[col][row - 1] !== 'undefined' &&
+      grid[col][row - 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col][row - 1].warning++;
+    }
+    if (
+      typeof grid[col + 1] !== 'undefined' &&
+      typeof grid[col + 1][row - 1] !== 'undefined' &&
+      grid[col + 1][row - 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col + 1][row - 1].warning++;
+    }
+    // MIDDLE
+    if (
+      typeof grid[col - 1] !== 'undefined' &&
+      typeof grid[col - 1][row] !== 'undefined' &&
+      grid[col - 1][row].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col - 1][row].warning++;
+    }
+    if (
+      typeof grid[col + 1] !== 'undefined' &&
+      typeof grid[col + 1][row] !== 'undefined' &&
+      grid[col + 1][row].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col + 1][row].warning++;
+    }
+    // BOTTOM
+    if (
+      typeof grid[col - 1] !== 'undefined' &&
+      typeof grid[col - 1][row + 1] !== 'undefined' &&
+      grid[col - 1][row + 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col - 1][row + 1].warning++;
+    }
+    if (
+      typeof grid[col][row + 1] !== 'undefined' &&
+      grid[col][row + 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col][row + 1].warning++;
+    }
+    if (
+      typeof grid[col + 1] !== 'undefined' &&
+      typeof grid[col + 1][row + 1] !== 'undefined' &&
+      grid[col + 1][row + 1].status !== cellStatus.CELL_MINE
+    ) {
+      grid[col + 1][row + 1].warning++;
+    }
   }
-
   return grid;
 };
 
