88f0fba88c9fe840d19189df77846c5a53900b77
slorenzo <SebastianLorenzo@gmail.com>
2019-01-18 17:42:33 -0300

    Added styled-components-breackpoint

--- src/global-style.js
+++ src/global-style.js
@@ -1,13 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
 
 const GlobalStyle = createGlobalStyle`
-  html, body, #root {
-    height: 100%;
+  html, body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  }
-
-  body {
     background-color: rgb(246, 247, 248);
+    font-size: 11px;
+    ${breakpoint('tablet')`
+      font-size: 16px;
+    `}
   }
 `;
 
