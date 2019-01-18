5f43fa6721d4f010b5247f509d72a33458184064
slorenzo <SebastianLorenzo@gmail.com>
2019-01-18 17:39:11 -0300

    Added service worker

--- src/index.js
+++ src/index.js
@@ -3,53 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Normalize } from 'styled-normalize';
 
+import * as serviceWorker from './serviceWorker';
 import GlobalStyle from './global-style';
 import configureStore from './store/configure';
 import Routes from './routes';
 
-import {
-  cellStatus,
-  ROW_BEGINNER,
-  COL_BEGINNER,
-  MINES_BEGINNER,
-  STOPWATCH_INITIAL_VALUE,
-} from './constants';
-
-import fillMineGrid from './utils/fill-mine-grid';
-import fillMultiArray from './utils/fill-multi-array';
-import fillWarningNumbers from './utils/fill-warning-numbers';
-import fillRandomBoolean from './utils/fill-random-boolean';
-
-const POSITION_QUANTITY = MINES_BEGINNER * 2;
-
-const positionMines = fillRandomBoolean(POSITION_QUANTITY, ROW_BEGINNER);
-
-const grid = fillMultiArray(ROW_BEGINNER, COL_BEGINNER, {
-  warning: 0,
-  status: cellStatus.CELL_INITIAL,
-});
-
-const gridWithMines = fillMineGrid(grid, positionMines);
-
-const gridWithWarningNumbers = fillWarningNumbers(gridWithMines, positionMines);
-
-const initialState = {
-  grid: {
-    grid: gridWithWarningNumbers,
-    rows: ROW_BEGINNER,
-    cols: COL_BEGINNER,
-    mines: MINES_BEGINNER,
-    level: 'Beginner',
-  },
-  stopwatch: {
-    text: STOPWATCH_INITIAL_VALUE,
-    seconds: 0,
-    minutes: 0,
-    hours: 0,
-  },
-};
-
-const store = configureStore(initialState);
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
@@ -59,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
