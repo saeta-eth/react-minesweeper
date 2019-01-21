60d0b202dd54c8cd199f3902688d80adf0cb1076
slorenzo <SebastianLorenzo@gmail.com>
2019-01-21 20:02:57 -0300

    Fixed position bombs

---
+++ src/utils/get-mines-position.js
@@ -0,0 +1,21 @@
+import chunk from 'lodash.chunk';
+
+const getMinesPosition = (quantity, max) => {
+  const positions = Array.from({ length: quantity * 2 }, () =>
+    Math.floor(Math.random() * max)
+  );
+
+  const mines = chunk(positions, 2);
+
+  // https://stackoverflow.com/a/44014849/1741027
+  const filter = Array.from(new Set(mines.map(JSON.stringify)), JSON.parse)
+    .length;
+
+  const isRepeat = filter === quantity;
+
+  if (isRepeat) getMinesPosition();
+
+  return mines;
+};
+
+export default getMinesPosition;
